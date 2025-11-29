81c8e84b177d16155979eb4c0bcb8da72a090654
jjackson112 <jjackson11211@gmail.com>
2025-11-28 21:36:37 -0500

    enqueue tailwind in functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
-
-add_action('wp_enqueue_scripts', function () {
+<?php
+// Enqueue scripts
+add_action('wp_enqueue_scripts', 'dorothy_enqueue_scripts');
+function dorothy_enqueue_scripts() {
     wp_enqueue_script(
         'mobile-menu',
         get_theme_file_uri('/assets/js/mobile-menu.js'),
@@ -7,4 +9,31 @@ add_action('wp_enqueue_scripts', function () {
         null,
         true
     );
-});
+}
+
+// Enqueue Tailwind
+add_action('wp_enqueue_scripts', 'dorothy_enqueue_styles');
+function dorothy_enqueue_styles() {
+    // front end
+    wp_enqueue_style(
+        'tailwind',
+        'https://cdn.tailwindcss.com',
+        [],
+        null
+    )
+
+    // Editor
+    add_theme_support('editor-styles');
+    add_editor_style('https://cdn.tailwindcss.com');
+}
+
+// Add theme support for features like custom logo
+add_action('after_setup_theme', 'dorothy_theme_setup');
+function dorothy_theme_setup() {
+    add_theme_support('custom-logo', [
+        'height'      => 48,
+        'width'       => 48,
+        'flex-height' => true,
+        'flex-width'  => true,
+    ]);
+}
