b9a135114d35a586b69f00050db2467c91203b70
jjackson112 <jjackson11211@gmail.com>
2025-12-03 16:08:41 -0500

    added pagination

---
+++ parts/pagination.php
@@ -0,0 +1,8 @@
+<div class="max-w-3xl mx-auto px-6 mt-12 pb-10 flex justify-between font-semibold">
+    <div class="hover:text-sky-300">
+        <?php previous_post_link('%link', '← Previous'); ?>
+    </div>
+    <div class="hover:text-sky-300">
+        <?php next_post_link('%link', 'Next →'); ?>
+    </div>
+</div>
