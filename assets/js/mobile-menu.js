ff579703f80d0457cf27f4878631ccd2a96d17bc
jjackson112 <jjackson11211@gmail.com>
2025-12-07 19:21:13 -0500

    querySelector not getElementById

--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -1,16 +1,18 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 document.addEventListener("DOMContentLoaded", () => {
-    const btn = document.getElementById("data-mobile-btn");
-    const menu = document.getElementById("data-mobile-menu");
-    const closeBtn = document.getElementById('data-mobile-close');
+    const btn = document.querySelector("[data-mobile-btn]");
+    const menu = document.querySelector("[data-mobile-menu]");
+    const closeBtn = document.querySelector('[data-mobile-close]');
+
+    if (!btn || !menu || !closeBtn) return;
 
     const openMenu = () => {
-        menu.classList.remove("hidden");
-        menu.classList.add("open");
+        menu.classList.remove("hidden",  "opacity-0", "max-h-0", "-translate-y-2");
+        menu.classList.add("opacity-100", "max-h-screen", "translate-y-0");
     };
 
     const closeMenu = () => {
-        menu.classList.remove("open");
+        menu.classList.add("opacity-100", "max-h-screen", "translate-y-0");
         setTimeout(() => {
             menu.classList.add("hidden");
         }, 300); // matches transition duration
@@ -18,5 +20,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
     btn.addEventListener("click", openMenu);
     closeBtn.addEventListener("click", closeMenu);
-
 });
