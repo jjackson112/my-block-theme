122ee287a1b3aff4731729f0fc62b12deddc39a6
jjackson112 <jjackson11211@gmail.com>
2025-12-12 15:11:45 -0500

    fixed syntax on event-details

--- templates/blocks/event-details.php
+++ templates/blocks/event-details.php
@@ -1,16 +1,25 @@
 <?php
-// Check if ACF is active
-if( function_exists('get_field') ):
-    $date = get_field('date');
-    $time = get_field('time')
-?>
+if ( function_exists('get_field') ) :
 
-<div class="event-details">
-    <?php if($date); ?>
-        <h4><i class="fa-regular fa-calendar text-sky-950"></i><?php echo esc_html($date); ?></h4>
+    $date = get_field('date');
+    $time = get_field('time');
+
+?>
+<div class="event-details space-y-4">
+
+    <?php if ( $date ) : ?>
+        <h4 class="text-lg font-semibold flex items-center gap-2">
+            <i class="fa-regular fa-calendar text-sky-950"></i>
+            <?php echo esc_html($date); ?>
+        </h4>
     <?php endif; ?>
-    <?php if($time); ?>
-        <p class="text-lg font-medium"><i class="fa-regular fa-clock text-sky-950"></i><?php echo esc_html($time); ?></p>
+
+    <?php if ( $time ) : ?>
+        <p class="text-lg flex items-center gap-2">
+            <i class="fa-regular fa-clock text-sky-950"></i>
+            <?php echo esc_html($time); ?>
+        </p>
     <?php endif; ?>
+
 </div>
 <?php endif; ?>
