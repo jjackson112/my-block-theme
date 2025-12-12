de9c45f28c940cb1654b82207fbf63ad845d0852
jjackson112 <jjackson11211@gmail.com>
2025-12-11 22:55:00 -0500

    acf registration - show acf fields

--- templates/blocks/event-details.php
+++ templates/blocks/event-details.php
@@ -9,7 +9,7 @@ if( function_exists('get_field') ):
     <?php if($date); ?>
         <h4><i class="fa-regular fa-calendar text-sky-950"></i><?php echo esc_html($date); ?></h4>
     <?php endif; ?>
-    <?php if(time); ?>
+    <?php if($time); ?>
         <p class="text-lg font-medium"><i class="fa-regular fa-clock text-sky-950"></i><?php echo esc_html($time); ?></p>
     <?php endif; ?>
 </div>
